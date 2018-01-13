f99cb476097e4cb2fce3094a89cac650e4850c6e
Luo Tao <lotreal@gmail.com>
2018-01-13 20:09:41 +0800

    FIX docker login

--- scripts/deploy-image.sh
+++ scripts/deploy-image.sh
@@ -5,6 +5,5 @@ source './scripts/infer.sh'
 
 docker tag ${APP_IMAGE} ${APP_IMAGE_LATEST}
 
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 docker push ${APP_IMAGE}
 docker push ${APP_IMAGE_LATEST}
