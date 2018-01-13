0316dd741edcecbf5573aa175caa00a672b02c54
Luo Tao <lotreal@gmail.com>
2018-01-13 20:00:32 +0800

    ADD readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# swain-go [![Build Status](https://travis-ci.org/hg2c/swain-go.svg?branch=master)](https://travis-ci.org/hg2c/swain-go)
+
+Swain Golang
+
+## development
+
+./scripts/docker.sh
--- scripts/deploy-image.sh
+++ scripts/deploy-image.sh
@@ -3,5 +3,8 @@ set -eu -o pipefail
 
 source './scripts/infer.sh'
 
+docker tag ${APP_IMAGE} ${APP_IMAGE_LATEST}
+
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 docker push ${APP_IMAGE}
+docker push ${APP_IMAGE_LATEST}
